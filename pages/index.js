b3b08518d930b7e69fad56887c25dfb8e283af82
Carlos Azaustre <cazaustre@gmail.com>
2021-07-09 18:10:17 +0200

    setup nextjs-auth0

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import { useUser } from "@auth0/nextjs-auth0";
 import { Center } from "@chakra-ui/react";
 import Head from "next/head";
 
 import { Footer, FormAdd, Header } from "@/components";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <div>
       <Head>
@@ -16,6 +22,11 @@ export default function Home() {
         <main>
           <Header />
           <FormAdd />
+          {user ? (
+            <a href="/api/auth/logout">Logout</a>
+          ) : (
+            <a href="/api/auth/login">Login</a>
+          )}
         </main>
       </Center>
 
---
+++ prisma/migrations/20210709153322_add_user_table/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Link` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Link" ADD COLUMN     "userId" INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Link" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
