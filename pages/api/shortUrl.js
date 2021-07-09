006bc1a8b93e892362c5e068ca05c6692e04ffd1
Carlos Azaustre <cazaustre@gmail.com>
2021-07-09 18:09:43 +0200

    refactor

--- pages/api/shortUrl.js
+++ pages/api/shortUrl.js
@@ -1,14 +1,12 @@
-import db from "@/lib/client";
+import { createShortLink } from "@/lib/db";
 
 export default async function handler(req, res) {
   const { url } = req.body;
   const shortUrl = Math.random().toString(36).substr(2, 7);
 
   try {
-    const data = await db.link.create({
-      data: { url, shortUrl },
-    });
-    console.log(data);
+    const data = await createShortLink(url, shortUrl);
+
     return res.status(200).json({ data });
   } catch (err) {
     console.log(err);
