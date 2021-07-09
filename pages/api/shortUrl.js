75a2beab4627064ffa777f09d55c4ee3e9d2b889
Carlos Azaustre <cazaustre@gmail.com>
2021-07-09 21:25:06 +0200

    store shortlink with userId

--- pages/api/shortUrl.js
+++ pages/api/shortUrl.js
@@ -1,11 +1,15 @@
-import { createShortLink } from "@/lib/db";
+import { getSession } from "@auth0/nextjs-auth0";
+
+import { createShortLink, getUserByEmail } from "@/lib/db";
 
 export default async function handler(req, res) {
+  const { user } = await getSession(req, res);
   const { url } = req.body;
   const shortUrl = Math.random().toString(36).substr(2, 7);
 
   try {
-    const data = await createShortLink(url, shortUrl);
+    const userDB = await getUserByEmail(user.email);
+    const data = await createShortLink(url, shortUrl, userDB.id);
 
     return res.status(200).json({ data });
   } catch (err) {
